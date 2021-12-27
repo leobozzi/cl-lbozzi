c0f8d6dffecb7891f5d5ec2d70d839a496da70d8
leobozzi <lbozzi@gmail.com>
2021-12-27 10:09:52 -0300

    Initial commit

--- lbsoft_default/__manifest__.py
+++ lbsoft_default/__manifest__.py
@@ -219,7 +219,7 @@
     ],
 
     'docker-images': [
-       'odoo lbozzi/odoo-docker-lb:12.0.ou',
+       'odoo lbozzi/odoo-docker-lb:12.0',
        'postgres postgres:11.1-alpine',
        'aeroo adhoc/aeroo-docs',
        'nginx nginx',
