68648a500d22d6c1a3defc16a3b04614edb7861b
leobozzi <lbozzi@gmail.com>
2022-02-12 11:36:18 -0300

    add odoo mate

--- lbsoft_default/__manifest__.py
+++ lbsoft_default/__manifest__.py
@@ -221,7 +221,7 @@
 
         # Odoomates
         # ==========================================================================================
-        # 'https://github.com/odoomates/odooapps odoomates-odooapps',
+        'https://github.com/odoomates/odooapps odoomates-odooapps',
 
         # Gabriela Rivero
         # ==========================================================================================
